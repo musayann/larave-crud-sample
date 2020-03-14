cb4b33a0fea349a5ffe7440b19929ac320558140
Yannick <musayann@octangrp.com>
2020-03-14 21:41:13 +0200

    Add sidebar to the dashboard

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,37 @@
+<div class="py-6">
+    @component('components.siderbar-item')
+
+        @slot('icon')
+        <i class="fas fa-tachometer-alt"></i>
+        @endslot
+
+        @slot('text')
+        Home
+        @endslot
+    @endcomponent
+
+    @component('components.siderbar-item')
+
+        @slot('icon')
+        <i class="fas fa-map-marker"></i>
+        @endslot
+
+        @slot('text')
+        Villages
+        @endslot
+    @endcomponent
+
+
+    @component('components.siderbar-item')
+
+        @slot('icon')
+        <i class="fas fa-map-marker"></i>
+        @endslot
+
+        @slot('text')
+        Villages
+        @endslot
+    @endcomponent
+
+
+</div>
